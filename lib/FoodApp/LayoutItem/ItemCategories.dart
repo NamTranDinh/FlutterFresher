efcff52a6fb1a97e093e2ff72416620d4beb6606
Nam Tran Dinh <trandinhnamnd0102@gmail.com>
2022-10-13 11:36:02 +0700

    Categorises's Food App -> change layout

--- lib/FoodApp/LayoutItem/ItemCategories.dart
+++ lib/FoodApp/LayoutItem/ItemCategories.dart
@@ -9,38 +9,52 @@ class ItemCategories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Color colorBg = categoriesModel.color;
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 3.0, vertical: 6.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.grey,
-            offset: Offset(0.0, 2.0), //(x,y)
-            blurRadius: 4.0,
-          ),
-        ],
-        gradient: LinearGradient(colors: [
-          colorBg.withOpacity(0.9),
-          colorBg,
-          colorBg.withOpacity(0.9)
-        ], begin: Alignment.topLeft, end: Alignment.topRight),
-        color: colorBg,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            categoriesModel.nameCategory,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              fontFamily: 'Montserrat_VariableFont_wght',
-              fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 3.0, vertical: 6.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              opacity: 0.5,
+              image: AssetImage(categoriesModel.img),
+              fit: BoxFit.cover),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(0.0, 2.0), //(x,y)
+              blurRadius: 4.0,
             ),
-          ),
-        ],
+          ],
+          gradient: LinearGradient(colors: [
+            colorBg.withOpacity(0.9),
+            colorBg,
+            colorBg.withOpacity(0.9)
+          ], begin: Alignment.topLeft, end: Alignment.topRight),
+          color: colorBg,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              categoriesModel.nameCategory,
+              style: const TextStyle(
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(1.0, 1.0),
+                    blurRadius: 2.0,
+                    color: Colors.white12,
+                  )
+                ],
+                color: Colors.white,
+                fontSize: 20,
+                fontFamily: 'Pacifico_Regular',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/FoodApp/Models/CategoriesModel.dart
+++ lib/FoodApp/Models/CategoriesModel.dart
@@ -4,6 +4,7 @@ class CategoriesModel{
   late int id;
   late String nameCategory;
   late Color color;
+  String img;
 
-  CategoriesModel({required this.id, required this.nameCategory, required this.color});
+  CategoriesModel({required this.id, required this.nameCategory, required this.color,required this.img});
 }
